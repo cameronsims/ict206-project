170fc2f66cf18f693e60e71934b5a4ca5891496c
Cameron Sims <59544722+cameronsims@users.noreply.github.com>
2024-11-04 11:46:51 +0800

    Final Commit
    Forgot a few files

--- data/network.js
+++ data/network.js
@@ -1,4 +1,8 @@
 const options = {
+	"configure": {
+		"enabled": true,
+		"filter": ["interaction", "manipulation", "physics"]
+	},
     "physics": {
         "forceAtlas2Based": {
             "springLength": 100,
